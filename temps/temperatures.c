07fd6a39737eeb4f6439872c5be89480ed264f9a
eduardkfc <gabidosh@gmail.com>
2017-10-30 14:05:16 +0300

    fix_it

--- temps/temperatures.c
+++ temps/temperatures.c
@@ -1,52 +1,70 @@
 #include <stdio.h>
 
-
-int main()
+void iError() /*Ошибка ввода*/
 {
+	printf("Usage sample:\n");
+	printf("(programm's path) t tChat\n");
+	printf("Where 't' for numeric data, 'tChar' for tempeture scale (C,K,F).\n");
+}
+
+float kelvin_s(double temp, double KELVIN, double FAHRENHEIT,double CELSIUS)
+{
+    KELVIN = temp;
+    FAHRENHEIT = 1.8*(temp-273.15)+32;
+    CELSIUS = temp-273.15;
+    printf("%.2f%s",KELVIN,"K\n");
+    printf("%.2f%s",FAHRENHEIT,"F\n");
+    printf("%.2f%s",CELSIUS,"C\n");
+}
+
+float celsius_s(double temp, double KELVIN, double FAHRENHEIT,double CELSIUS)
+{
+    CELSIUS = temp; 
+    KELVIN = temp+273.15;
+    FAHRENHEIT = (temp*1.8)+32;
+    printf("%.2f%s",CELSIUS,"C\n");
+    printf("%.2f%s",KELVIN,"K\n");
+    printf("%.2f%s",FAHRENHEIT,"F\n"); 
+}
+float fahrenheit_s(double temp, double KELVIN, double FAHRENHEIT,double CELSIUS)
+{
+    FAHRENHEIT = temp;
+    CELSIUS = (temp-32)*(5/9); 
+    KELVIN = (temp+459.67)*(5/9);
+    printf("%.2f%s",CELSIUS,"C\n");
+    printf("%.2f%s",KELVIN,"K\n");
+    printf("%.2f%s",FAHRENHEIT,"F\n");
+}
+int main(int argc, char* argv[])
+{
+/* argv[0] = "gcc" */
+/* argv[1] = "temp..." */
     double temp; /*Storage of temp */
     double KELVIN,CELSIUS,FAHRENHEIT; /* Results */
     char system; /* Temperature system */
-    printf("Enter temperature "); /* Output Message */
-    scanf("%lg",&temp); /*Enter a temperature */
-    printf("\nEnter system K-kelvin, C-celsius, F-fahrenheit\n"); /* Ask of user for system */
-    /* scanf(" %c",&system);  Enter a system */ 
-    getchar();
-    system = getchar();
-    if (system == 'K') 
+    if (argc<2) { iError(); return 0; }
+    sscanf(argv[1], "%lf", &temp);
+    if (argc==3) { system = argv[2][0]; }
+ 
+    if (system == 'K' || system == 'k')
     {
-        /* Kelvin system entered */
-        KELVIN = temp;
-        FAHRENHEIT = 1.8*(temp-273.15)+32;
-        CELSIUS = temp-273.15;
-        printf("%.2f%s",KELVIN,"K\n");
-        printf("%.2f%s",FAHRENHEIT,"F\n");
-        printf("%.2f%s",CELSIUS,"C\n");
+        kelvin_s(temp,KELVIN,FAHRENHEIT,CELSIUS);
     }
-    else if (system == 'C')
+    else if (system == 'C' || system == 'c')
     {
-        CELSIUS = temp; 
-        KELVIN = temp+273.15;
-        FAHRENHEIT = (temp*1.8)+32;
-        printf("%.2f%s",CELSIUS,"C\n");
-        printf("%.2f%s",KELVIN,"K\n");
-        printf("%.2f%s",FAHRENHEIT,"F\n"); 
+        celsius_s(temp,KELVIN,FAHRENHEIT,CELSIUS);
     }
-    else if (system == 'F') 
+    else if (system == 'F' || system == 'f') 
     { 
-        FAHRENHEIT = temp;
-        CELSIUS = (temp-32)*(5/9); 
-        KELVIN = (temp+459.67)*(5/9);
-        printf("%.2f%s",CELSIUS,"C\n");
-        printf("%.2f%s",KELVIN,"K\n");
-        printf("%.2f%s",FAHRENHEIT,"F\n"); 
+         fahrenheit_s(temp,KELVIN,FAHRENHEIT,CELSIUS);
     }
     else
     {
-        CELSIUS = temp; 
-        KELVIN = temp+273.15;
-        FAHRENHEIT = (temp+32)*1.8;
-        printf("%.2f%s",CELSIUS,"C\n");
-        printf("%.2f%s",KELVIN,"K\n");
-        printf("%.2f%s",FAHRENHEIT,"F\n");
+        printf("default kelvin\n");
+        kelvin_s(temp,KELVIN,FAHRENHEIT,CELSIUS);
+        printf("default celsius");
+        celsius_s(temp,KELVIN,FAHRENHEIT,CELSIUS);
+        printf("default fahrenheit");
+        fahrenheit_s(temp,KELVIN,FAHRENHEIT,CELSIUS);
     }    
 }
